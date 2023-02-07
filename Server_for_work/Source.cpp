0c825d064694d5d8a967ecbf63367a39229c19f5
Maxim-Vasuytin <72349897+Maxim-Vasuytin@users.noreply.github.com>
2023-02-07 20:11:08 +0300

    Добавил потоки

--- Server_for_work/Source.cpp
+++ Server_for_work/Source.cpp
@@ -4,20 +4,20 @@
 #include <algorithm>
 #include <string>
 #include <sstream>
+#include <thread>
+#include <windows.h>
 
 using namespace std;
+int n = 3, i = 0, sum = 0;				//64 символа на ввод
+string ss;
+string intStr;
 
-void main()
+string first()
 {
-	setlocale(LC_ALL, "rus");
-	int n = 3, i = 0, sum = 0;				//64 символа на ввод
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	vector <int> data(n);
-	string ss;
-	string intStr;
-
-
-	//Проверить принадлежность символа к числовым значениям
-
+	setlocale(LC_ALL, "rus");
 	cout << "Enter only numeric values: " << flush << endl;
 	for (int i = 0; i < n; i++)
 	{
@@ -29,15 +29,14 @@ void main()
 			break;
 		}
 	}
-
 	//Сортировка по убыванию
 	sort(begin(data), end(data), greater<>());
 
 	//Изменение чётных чисел
-	for (int i = 0; i<n;i++)
+	for (int i = 0; i < n; i++)
 	{
 		stringstream ss;
-		if (data[i]%2 == 0)
+		if (data[i] % 2 == 0)
 		{
 			intStr += "КВ ";
 		}
@@ -45,16 +44,34 @@ void main()
 		{
 			ss << data[i];
 			intStr += ss.str() + " ";
-		}		
+		}
 	}
 	cout << "\nМассив символов:\n" << intStr;
+	return intStr;
+}
 
-
-	//2 программа
+//2 программа
+int second(string intStr)
+{
+	//Придумать, как переводить из строки в массив int
 	cout << "\n\nСумма обработанного массива = " << sum << endl << flush;
+	return 0;
+}
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	thread t1(first);
+	t1.join();
+	cout << "\nаХХХАХАХХА:\t" << intStr;
+	thread t2(second, intStr);
+	t2.join();
+	return 0;
+	}
 
 	//Реализовать потоки. Один вечно считывает данные, другой вечно печатает. Синхронизировать
 	//считать интовый массив, потом собрать массив строк и потом в массив
 
 	//копировать инты из стринга в массив интовый и с ним уже работать
-}
